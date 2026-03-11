062d3fa748f9fb6d46a57162c04b0a3aed9a368f
Dhanush292006 <dhanushs99777@gmail.com>
2026-03-11 20:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ model = joblib.load("csat_model.pkl")
 
 st.title("DeepCSAT - Customer Satisfaction Prediction")
 
-response_time = st.number_input("Response Time", min_value=0.0)
-handling_time = st.number_input("Handling Time", min_value=0.0)
+response_time = st.number_input("Response Time",min_value=0.0)
+handling_time = st.number_input("Handling Time",min_value=0.0)
 
 if st.button("Predict CSAT"):
 
-    features = np.array([[response_time, handling_time]])
+    features = np.array([[response_time,handling_time]])
 
     prediction = model.predict(features)
 
